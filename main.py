ed75aab75af9150ffb6549b50a195817ad7d796e
Амир <mixenix@yandex.ru>
2023-02-08 19:40:56 +0500

    на pygame PG_UP, PG_DOWN

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import pygame
+import copy
 from io import BytesIO
 import requests as requests
 # Этот класс поможет нам сделать картинку из потока байт
@@ -13,36 +14,62 @@ from PIL import Image
 
 pygame.init()
 toponym_coodrinates = input('Введите координаты: ').split(',')
-z = input('Введите масштаб: ')
+zinit = int(input('Введите масштаб: '))
 # Долгота и широта:
+dct_resp = {}
+lst_resp = []
 toponym_longitude, toponym_lattitude = toponym_coodrinates
-
-# Собираем параметры для запроса к StaticMapsAPI:
+map_api_server = "http://static-maps.yandex.ru/1.x/"
 map_params = {
     "ll": ",".join([toponym_longitude, toponym_lattitude]),
-    "z": z,
+    "z": str(zinit),
     "l": "map"
 }
-lst_resp = []
-map_api_server = "http://static-maps.yandex.ru/1.x/"
-# ... и выполняем запрос
 response = requests.get(map_api_server, params=map_params)
-lst_resp.append(response)
 map_file = "map.png"
 with open(map_file, "wb") as file:
-    file.write(lst_resp[0].content)
+    file.write(response.content)
 running = True
-cnt = 0
+cnt = zinit
 screen = pygame.display.set_mode((600, 450))
 screen.blit(pygame.image.load(map_file), (0, 0))
 pygame.display.flip()
+os.remove(map_file)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
-            os.remove(map_file)
             sys.exit()
-pygame.quit()
-os.remove(map_file)
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_PAGEUP:
+                if cnt + 1 in range(0, 18):
+                    cnt += 1
+                    map_params = {
+                        "ll": ",".join([toponym_longitude, toponym_lattitude]),
+                        "z": str(cnt),
+                        "l": "map"
+                    }
+                    response = requests.get(map_api_server, params=map_params)
+                    map_file = "map.png"
+                    with open(map_file, "wb") as file:
+                        file.write(response.content)
+                    screen.blit(pygame.image.load(map_file), (0, 0))
+                    pygame.display.flip()
+                    os.remove(map_file)
+            if event.key == pygame.K_PAGEDOWN:
+                if cnt - 1 in range(0, 18):
+                    cnt -= 1
+                    map_params = {
+                        "ll": ",".join([toponym_longitude, toponym_lattitude]),
+                        "z": str(cnt),
+                        "l": "map"
+                    }
+                    response = requests.get(map_api_server, params=map_params)
+                    map_file = "map.png"
+                    with open(map_file, "wb") as file:
+                        file.write(response.content)
+                    screen.blit(pygame.image.load(map_file), (0, 0))
+                    pygame.display.flip()
+                    os.remove(map_file)
 sys.exit()
