0a2180d01e75de4df9787dbb3d8ca6b1ccf940f8
Амир <mixenix@yandex.ru>
2023-02-08 18:52:00 +0500

    на pygame

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import pygame
 from io import BytesIO
 import requests as requests
 # Этот класс поможет нам сделать картинку из потока байт
@@ -9,7 +11,7 @@ from PIL import Image
 # Тогда запрос к геокодеру формируется следующим образом:
 
 
-
+pygame.init()
 toponym_coodrinates = input('Введите координаты: ').split(',')
 z = input('Введите масштаб: ')
 # Долгота и широта:
@@ -21,12 +23,26 @@ map_params = {
     "z": z,
     "l": "map"
 }
-
+lst_resp = []
 map_api_server = "http://static-maps.yandex.ru/1.x/"
 # ... и выполняем запрос
 response = requests.get(map_api_server, params=map_params)
-
-Image.open(BytesIO(
-    response.content)).show()
-# Создадим картинку
-# и тут же ее покажем встроенным просмотрщиком операционной системы
+lst_resp.append(response)
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(lst_resp[0].content)
+running = True
+cnt = 0
+screen = pygame.display.set_mode((600, 450))
+screen.blit(pygame.image.load(map_file), (0, 0))
+pygame.display.flip()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+            os.remove(map_file)
+            sys.exit()
+pygame.quit()
+os.remove(map_file)
+sys.exit()
