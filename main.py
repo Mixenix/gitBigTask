df954dc4fb13fbd62d9f81a7dd39f97d3390ec36
Амир <mixenix@yandex.ru>
2023-02-08 21:10:49 +0500

    третья задача

--- main.py
+++ main.py
@@ -1,28 +1,19 @@
-import sys
 import os
+import sys
+
 import pygame
-import copy
-from io import BytesIO
 import requests as requests
-# Этот класс поможет нам сделать картинку из потока байт
-from PIL import Image
-
-# Пусть наше приложение предполагает запуск:
-# python search.py Москва, ул. Ак. Королева, 12
-# Тогда запрос к геокодеру формируется следующим образом:
-
 
 pygame.init()
 toponym_coodrinates = input('Введите координаты: ').split(',')
-zinit = int(input('Введите масштаб: '))
-# Долгота и широта:
+zinit = float(input('Введите масштаб: (0.xx) '))
 dct_resp = {}
 lst_resp = []
-toponym_longitude, toponym_lattitude = toponym_coodrinates
+coords_long, coords_lat = toponym_coodrinates
 map_api_server = "http://static-maps.yandex.ru/1.x/"
 map_params = {
-    "ll": ",".join([toponym_longitude, toponym_lattitude]),
-    "z": str(zinit),
+    "ll": ",".join([coords_long, coords_lat]),
+    "spn": ','.join([str(zinit), str(zinit)]),
     "l": "map"
 }
 response = requests.get(map_api_server, params=map_params)
@@ -31,6 +22,10 @@ with open(map_file, "wb") as file:
     file.write(response.content)
 running = True
 cnt = zinit
+countleft = 1
+countright = 1
+countup = 1
+countdown = 1
 screen = pygame.display.set_mode((600, 450))
 screen.blit(pygame.image.load(map_file), (0, 0))
 pygame.display.flip()
@@ -43,11 +38,11 @@ while running:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_PAGEUP:
-                if cnt + 1 in range(0, 18):
-                    cnt += 1
+                if cnt / 1.5 >= 0 and cnt / 1.5 <= 90.0:
+                    cnt /= 1.5
                     map_params = {
-                        "ll": ",".join([toponym_longitude, toponym_lattitude]),
-                        "z": str(cnt),
+                        "ll": ",".join([coords_long, coords_lat]),
+                        "spn": ','.join([str(cnt), str(cnt)]),
                         "l": "map"
                     }
                     response = requests.get(map_api_server, params=map_params)
@@ -58,11 +53,71 @@ while running:
                     pygame.display.flip()
                     os.remove(map_file)
             if event.key == pygame.K_PAGEDOWN:
-                if cnt - 1 in range(0, 18):
-                    cnt -= 1
+                if cnt * 1.5 >= 0 and cnt * 1.5 <= 90.0:
+                    cnt *= 1.5
                     map_params = {
-                        "ll": ",".join([toponym_longitude, toponym_lattitude]),
-                        "z": str(cnt),
+                        "ll": ",".join([coords_long, coords_lat]),
+                        "spn": ','.join([str(cnt), str(cnt)]),
+                        "l": "map"
+                    }
+                    response = requests.get(map_api_server, params=map_params)
+                    map_file = "map.png"
+                    with open(map_file, "wb") as file:
+                        file.write(response.content)
+                    screen.blit(pygame.image.load(map_file), (0, 0))
+                    pygame.display.flip()
+                    os.remove(map_file)
+            if event.key == pygame.K_DOWN:
+                if float(coords_lat) - cnt * 2 <= 180 and float(coords_lat) - cnt * 2 >= -180:
+                    coords_lat = str(float(coords_lat) - cnt * 2)
+                    map_params = {
+                        "ll": ",".join([coords_long, coords_lat]),
+                        "spn": ','.join([str(cnt), str(cnt)]),
+                        "l": "map"
+                    }
+                    response = requests.get(map_api_server, params=map_params)
+                    map_file = "map.png"
+                    with open(map_file, "wb") as file:
+                        file.write(response.content)
+                    screen.blit(pygame.image.load(map_file), (0, 0))
+                    pygame.display.flip()
+                    os.remove(map_file)
+            if event.key == pygame.K_UP:
+                if float(coords_lat) + cnt * 2 <= 180 and float(coords_lat) + cnt * 2 >= -180:
+                    coords_lat = str(float(coords_lat) + cnt * 2)
+                    map_params = {
+                        "ll": ",".join([coords_long, coords_lat]),
+                        "spn": ','.join([str(cnt), str(cnt)]),
+                        "l": "map"
+                    }
+                    response = requests.get(map_api_server, params=map_params)
+                    map_file = "map.png"
+                    with open(map_file, "wb") as file:
+                        file.write(response.content)
+                    screen.blit(pygame.image.load(map_file), (0, 0))
+                    pygame.display.flip()
+                    os.remove(map_file)
+            if event.key == pygame.K_LEFT:
+                if float(coords_long) - cnt * 2 <= 180 and float(coords_long) - cnt * 2 >= -180:
+                    coords_long = str(float(coords_long) - cnt * 2)
+                    map_params = {
+                        "ll": ",".join([coords_long, coords_lat]),
+                        "spn": ','.join([str(cnt), str(cnt)]),
+                        "l": "map"
+                    }
+                    response = requests.get(map_api_server, params=map_params)
+                    map_file = "map.png"
+                    with open(map_file, "wb") as file:
+                        file.write(response.content)
+                    screen.blit(pygame.image.load(map_file), (0, 0))
+                    pygame.display.flip()
+                    os.remove(map_file)
+            if event.key == pygame.K_RIGHT:
+                if float(coords_long) + cnt * 2 <= 180 and float(coords_long) + cnt * 2 >= -180:
+                    coords_long = str(float(coords_long) + cnt * 2)
+                    map_params = {
+                        "ll": ",".join([coords_long, coords_lat]),
+                        "spn": ','.join([str(cnt), str(cnt)]),
                         "l": "map"
                     }
                     response = requests.get(map_api_server, params=map_params)
