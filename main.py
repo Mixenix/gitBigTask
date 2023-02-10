57d80d706a17ae53c8804bb0046f1e8253a77953
Амир <mixenix@yandex.ru>
2023-02-10 21:23:25 +0500

    шестая задача почти без глобалов

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from pygame_widgets.dropdown import Dropdown
 import requests as requests
 from pygame_widgets.textbox import TextBox
 
+
 pygame.init()
 
 toponym_coodrinates = input('Введите координаты: ').split(',')
@@ -15,14 +16,15 @@ zinit = float(input('Введите масштаб: (0.xx) '))
 
 dct_resp = {}
 lst_resp = []
+points = []
 
-map = 'map'
+mapBASE = ('map', 'sat', 'sat,skl')
+map = mapBASE[0]
 coordsxy = [coords_long, coords_lat] = toponym_coodrinates
-
 map_api_server = "http://static-maps.yandex.ru/1.x/"
 
 map_params = {
-    "ll": ",".join([coords_long, coords_lat]),
+    "ll": ",".join(coordsxy),
     "spn": ','.join([str(zinit), str(zinit)]),
     "l": "map"
 }
@@ -33,56 +35,58 @@ with open(map_file, "wb") as file:
     file.write(response.content)
 running = True
 cnt = zinit
-countleft = 1
-countright = 1
-countup = 1
-countdown = 1
 screen = pygame.display.set_mode((600, 450))
 screen.blit(pygame.image.load(map_file), (0, 0))
 pygame.display.flip()
 os.remove(map_file)
 
 
-def apply_value(coordsxy, cnt=0.1):
+def apply_value():
     global map
     value = dropdown.getSelected()
-    if value == 'map':
+    if value:
+        map = value[::]
+    else:
         map = 'map'
-    elif value == 'sat':
-        map = 'sat'
-    elif value == 'sat,skl':
-        map = 'sat,skl'
-    update_map(screen, coordsxy, cnt=cnt)
+    # if value == 'map':
+    #     map = 'map'
+    # elif value == 'sat':
+    #     map = 'sat'
+    # elif value == 'sat,skl':
+    #     map = 'sat,skl'
+    update_map(screen, coordsxy, points, cnt=cnt)
 
 
 # активируется клавишей enter или кнопкой 'искать'
-def output_text(coordsxy):
-    global map
+def output_text():
+    global coordsxy
     srk = textbox.getText()
-    geo_request = "http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode" \
-                  f"={srk}&format=json"
-    resp = requests.get(geo_request)
-    json_response = resp.json()
-    toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-    toponym_coords = [float(f) for f in toponym['Point']['pos'].split(' ')]
-    coords_long = str(toponym_coords[0])
-    coords_lat = str(toponym_coords[1])
-    coordsxy = [coords_long, coords_lat]
-    update_map(screen, coordsxy, cnt=cnt, top_coords=toponym_coords)
+    if srk:
+        geo_request = "http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode" \
+                      f"={srk}&format=json"
+        resp = requests.get(geo_request)
+        json_response = resp.json()
+        toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+        toponym_coords = [float(f) for f in toponym['Point']['pos'].split(' ')]
+        coords_long = str(toponym_coords[0])
+        coords_lat = str(toponym_coords[1])
+        coordsxy = [coords_long, coords_lat]
+        points.append(','.join([str(toponym_coords[0]), str(toponym_coords[1]), 'pm2rdm']))
+        update_map(screen, coordsxy, points, cnt=cnt)
 
 
-def update_map(screen, coordsxy, cnt=0.1, top_coords=None):
+def update_map(screen, coordsxy, points, cnt=0.1):
     global map
-    if top_coords is not None:
+    if len(points) > 0:
         map_params = {
             "ll": ",".join(coordsxy),
             "spn": ','.join([str(cnt), str(cnt)]),
             "l": map,
-            "pt": ','.join([str(top_coords[0]), str(top_coords[1]), 'pm2rdm'])
+            "pt": ','.join(['~'.join(points)])
         }
     else:
         map_params = {
-            "ll": ",".join([coords_long, coords_lat]),
+            "ll": ",".join(coordsxy),
             "spn": ','.join([str(cnt), str(cnt)]),
             "l": map
         }
@@ -133,31 +137,32 @@ while running:
             if event.key == pygame.K_PAGEUP:
                 if cnt / 1.5 >= 0:
                     cnt /= 1.5
-                    update_map(screen, coordsxy, cnt=cnt)
+                    update_map(screen, coordsxy, points, cnt=cnt)
+            coords_long, coords_lat = coordsxy
             if event.key == pygame.K_PAGEDOWN:
                 if cnt * 1.5 >= 0:
                     cnt *= 1.5
-                    update_map(screen, coordsxy, cnt=cnt)
+                    update_map(screen, coordsxy, points, cnt=cnt)
             if event.key == pygame.K_DOWN:
                 if (float(coords_lat) - cnt * 2 <= 90) and (float(coords_lat) - cnt * 2) >= -90:
                     coords_lat = str(float(coords_lat) - cnt * 2)
                     coordsxy = [coords_long, coords_lat]
-                    update_map(screen, coordsxy, cnt=cnt)
+                    update_map(screen, coordsxy, points, cnt=cnt)
             if event.key == pygame.K_UP:
                 if (float(coords_lat) + cnt * 2 <= 90) and (float(coords_lat) + cnt * 2) >= -90:
                     coords_lat = str(float(coords_lat) + cnt * 2)
                     coordsxy = [coords_long, coords_lat]
-                    update_map(screen, coordsxy, cnt=cnt)
+                    update_map(screen, coordsxy, points, cnt=cnt)
             if event.key == pygame.K_LEFT:
                 if float(coords_long) - cnt * 2 < 180 and float(coords_long) - cnt * 2 > -180:
                     coords_long = str(float(coords_long) - cnt * 2)
                     coordsxy = [coords_long, coords_lat]
-                    update_map(screen, coordsxy, cnt=cnt)
+                    update_map(screen, coordsxy, points, cnt=cnt)
             if event.key == pygame.K_RIGHT:
                 if float(coords_long) + cnt * 2 < 180 and float(coords_long) + cnt * 2 > -180:
                     coords_long = str(float(coords_long) + cnt * 2)
                     coordsxy = [coords_long, coords_lat]
-                    update_map(screen, coordsxy, cnt=cnt)
+                    update_map(screen, coordsxy, points, cnt=cnt)
     pygame_widgets.update(events)
     pygame.display.update()
 sys.exit()
